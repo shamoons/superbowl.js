db10ade041e49088168fadc8b2aacbbd491bebc0
David Newell <beta@alumni.rice.edu>
2012-01-24 20:50:29 -0500

    emiting click event when the canvas is clicked

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -1,18 +1,16 @@
 (function() {
 	$(document).ready(function() {
+    var socket = io.connect('http://localhost:3000');
 
 		drawInit();
     $('canvas').click(canvasClicked);
 			
     function canvasClicked(e) {
-      alert("clicked");
 		  $('#status').html(e.pageX +', '+ e.pageY);
-		  ix = e.pageX;
-		  iy = e.pageY;
 		  console.log(e.pageX +', '+ e.pageY);
+      socket.emit('click', { x: e.pageX, y: e.pageY });
     }
 
-    var socket = io.connect('http://localhost:3000');
     socket.on('news', function (data) {
     	console.log(data);
       	socket.emit('my other event', { my: 'data' });
