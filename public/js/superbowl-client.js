55cba76b608446a8d1f50d830d8e0b8e79908b09
Dave Lee <dave@thinkbinder.com>
2012-01-24 20:34:16 -0500

    format js

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -1,22 +1,22 @@
 (function() {
 	$(document).ready(function() {
-      // $('#canvasWrapper')
+      	// $('#canvasWrapper')
 
-      $('canvas').click(canvasClicked);
+      	$('canvas').click(canvasClicked);
 			
-      function canvasClicked() {
-        alert("clicked");
-		$('#status').html(e.pageX +', '+ e.pageY);
-		ix = e.pageX;
-		iy = e.pageY;
-		console.log(e.pageX +', '+ e.pageY);
-      }
+      	function canvasClicked() {
+        	alert("clicked");
+			$('#status').html(e.pageX +', '+ e.pageY);
+			ix = e.pageX;
+			iy = e.pageY;
+			console.log(e.pageX +', '+ e.pageY);
+      	}
 
-    var socket = io.connect('http://localhost:3000');
-    socket.on('news', function (data) {
-      console.log(data);
-      socket.emit('my other event', { my: 'data' });
-    });
-  });
+    	var socket = io.connect('http://localhost:3000');
+    	socket.on('news', function (data) {
+      		console.log(data);
+      		socket.emit('my other event', { my: 'data' });
+    	});
+  	});
 		
 })();
