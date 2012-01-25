539e41bce0e4f4169d11409e932764d0e23ac0b1
Shamoon Siddiqui <EmailShamoon@gmail.com>
2012-01-24 20:07:55 -0500

    SocketIO works

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -2,6 +2,12 @@
     jQuery(function($) {
 		$(document).ready(function() {
 			console.log('loaded');
+
+      var socket = io.connect('http://localhost:3000');
+      socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+      });
 			
 			$('#canvasWrapper')
 			
