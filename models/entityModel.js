84813512c407c3890770467beba53d455f9a83c1
Matt Walters <mattwalters5@gmail.com>
2012-01-24 19:45:50 -0500

    updated package.json, entityMode.js

--- models/entityModel.js
+++ models/entityModel.js
@@ -1,20 +1,17 @@
 var mongoose = require('mongoose');
+
 var LocationSchema = require('./schemas/locationSchema');
-
-/*
-	Remember the Schema.Types.Mixed property definition requires that code similar 
-	to the following be used whenever saving this entity:  
-
-	entity.data = { x: [3, 4, { y: "changed" }] };
-	entity.markModified('data');
-	entity.save(); // entity will now get saved
-*/
 
 var EntitySchema = new mongoose.Schema({
 	addedOn     : Date
   , data   	 	: Schema.Types.Mixed 
   , location	: { LocationSchema, index: true }
   , user_id   : String
+});
+
+Entity.pre('save', function (next) {
+  this.markModified('data');
+  next();
 });
 
 exports.Entity = mongoose.model 'Entity', EntitySchema
