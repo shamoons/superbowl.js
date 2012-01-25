10785396e49efa3afd94cc998ce30f0c4644d89b
Dave Lee <dave@thinkbinder.com>
2012-01-24 20:32:12 -0500

    click position

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -8,6 +8,10 @@
 			
       function canvasClicked() {
         alert("clicked");
+		$('#status').html(e.pageX +', '+ e.pageY);
+		ix = e.pageX;
+		iy = e.pageY;
+		console.log(e.pageX +', '+ e.pageY);
       }
 
     var socket = io.connect('http://localhost:3000');
