cc5fea7c7928622ff3f4d62fb12c56a184b5f031
Shamoon Siddiqui <EmailShamoon@gmail.com>
2012-01-24 20:59:56 -0500

    Cleaned up index file

--- config/index.js
+++ config/index.js
@@ -5,6 +5,4 @@ env = process.env.NODE_ENV || 'base';
 confData = fs.readFileSync(__dirname + '/' + env + '.json', 'ascii');
 confData = JSON.parse(confData);
 
-//baseConf = JSON.parse(fs.readFileSync(__dirname + '/base.json', 'ascii'))
-
 module.exports = confData;
