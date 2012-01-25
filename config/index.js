2e8578e0d535029d6634e15c490170a0f0df2aa0
Shamoon Siddiqui <EmailShamoon@gmail.com>
2012-01-24 20:54:17 -0500

    Conf'd MongoDB

--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
 fs = require('fs');
 
-baseConf = JSON.parse(fs.readFileSync(__dirname + '/base.json', 'ascii'))
 
-module.exports = baseConf;
+env = process.env.NODE_ENV || 'base';
+confData = fs.readFileSync(__dirname + '/' + env + '.json', 'ascii');
+confData = JSON.parse(confData);
+
+//baseConf = JSON.parse(fs.readFileSync(__dirname + '/base.json', 'ascii'))
+
+module.exports = confData;
