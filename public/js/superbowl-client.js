3d1964a55917af797556b871eb531165de4d1538
Dave Lee <dave@thinkbinder.com>
2012-01-24 21:33:39 -0500

    asd

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -1,29 +1,29 @@
 (function() {
 	$(document).ready(function() {
-    window.socket = io.connect('http://localhost:3000');
+    	window.socket = io.connect('http://localhost:3000');
 
 		drawInit();
-    $('canvas').click(canvasClicked);
+    	$('canvas').click(canvasClicked);
 
-    function canvasClicked(e) {
-		  $('#status').html(e.pageX +', '+ e.pageY);
-		  console.log(e.pageX +', '+ e.pageY);
-		  ix = e.pageX;
-		  iy = e.pageY;
-		  
-		  var ctx = this.getContext("2d");
-      //draw a circle
-      ctx.beginPath();
-      ctx.arc(ix, iy, 10, 0, Math.PI*2, true); 
-      ctx.closePath();
-      ctx.fill();
-    }
+    	function canvasClicked(e) {
+			$('#status').html(e.pageX +', '+ e.pageY);
+			console.log(e.pageX +', '+ e.pageY);
+			ix = e.pageX;
+			iy = e.pageY;
+
+			var ctx = this.getContext("2d");
+		     //draw a circle
+			ctx.beginPath();
+			ctx.arc(ix, iy, 10, 0, Math.PI*2, true); 
+			ctx.closePath();
+			ctx.fill();
+		 }
 
     window.socket.on('news', function (data) {
     	console.log(data);
       window.socket.emit('my other event', { my: 'data' });
-    });
-  });
+    	});
+ 	});
 		
 	function drawInit() {
 		var c=document.getElementById("canvas");
