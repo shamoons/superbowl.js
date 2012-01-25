c05072ac02fa8aa11bf702347bc09e4df72b1f15
David Newell <beta@alumni.rice.edu>
2012-01-24 20:41:45 -0500

    formatting changes

--- public/js/superbowl-client.js
+++ public/js/superbowl-client.js
@@ -1,16 +1,15 @@
 (function() {
 	$(document).ready(function() {
-      // $('#canvasWrapper')
 
-      $('canvas').click(canvasClicked);
+    $('canvas').click(canvasClicked);
 			
-      function canvasClicked() {
-        alert("clicked");
-		$('#status').html(e.pageX +', '+ e.pageY);
-		ix = e.pageX;
-		iy = e.pageY;
-		console.log(e.pageX +', '+ e.pageY);
-      }
+    function canvasClicked(e) {
+      alert("clicked");
+		  $('#status').html(e.pageX +', '+ e.pageY);
+		  ix = e.pageX;
+		  iy = e.pageY;
+		  console.log(e.pageX +', '+ e.pageY);
+    }
 
     var socket = io.connect('http://localhost:3000');
     socket.on('news', function (data) {
