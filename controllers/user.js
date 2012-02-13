a7b8b62680cef10d418291666a9a8bfde670a801
Jonas Gebhardt <jonas.gebhardt@gmail.com>
2012-02-12 22:25:26 -0500

    user login & registration working on backend

--- controllers/user.js
+++ controllers/user.js
@@ -1,30 +1,27 @@
 User = require('../models/userModel');
 
+
 exports.authenticate = function(data, onSuccess, onFailure) {
-  console.log('looking for users');
   User.findOne({'email': data.email}, function (err, doc) {
     if (err) {
-      console.log ('error: ');
-      console.log(err);
+      onFailure(err, {message: 'Error querying users.'});
     } else {
       if (doc != null ) {
-        if (doc.password === data.password) {
-          onSuccess('Login successful!');        
+        if (doc.passwordEquals(data.password)) {
+          onSuccess('Login successful!', {message: 'Welcome back!'});        
         } else {
-          onFailure('Wrong Password');
+          onFailure('Wrong Password', {message: 'Incorrect Password.'});
         }
       } else {
-        console.log('no user found for email "' + data.email + '", creating new one');
         var u = new User({email: data.email, password: data.password});
           u.save(function(err){
             if (err) {
-              onFailure(err);
+              onFailure(err, {message: 'Error saving user data.'});
             } else {
-              console.log('save successful');
+              onSuccess('Success: created new User', {message: 'Success! We just created a new account for you.'})
             }
           });
       } 
     }
   }); 
-
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,47 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
-    email       : {	type: String
-	    		  , validate: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'not a valid email address']}
-  , password    : String
-  , createdOn   : { type: Date, default: Date.now }
+    email       : { type: String
+	    		  , validate: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'not a valid email address']
+	    		  , index: { unique: true }
+	    		  , set: toLower}
+	    		  //TODO validate that email address is not empty
+
+  , password    : { type: String
+  				  }
+
+  , salt		: { type: String
+	  			  , default: generateSalt}
+
+  , createdOn   : { type: Date
+	  			  , default: Date.now }
 });
+
+
+function toLower (str) {
+  return str.toLowerCase();
+}
+
+function encrypt (plaintext, salt) {
+	var hash = crypto.createHash('sha512');
+	hash.update(plaintext + salt);
+	return hash.digest('hex');
+}
+
+function generateSalt() {
+	return Math.round((new Date().valueOf() * Math.random())) + '';
+}
+
+UserSchema.path('password').set(function (plaintext) {
+	return encrypt(plaintext, this.salt);
+});
+
+UserSchema.method('passwordEquals', function(test_password) {
+	return encrypt(test_password, this.salt) === this.password;
+});
+
+
 
 
 module.exports = mongoose.model('User', UserSchema)
